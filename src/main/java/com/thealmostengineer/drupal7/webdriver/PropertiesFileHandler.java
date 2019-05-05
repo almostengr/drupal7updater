ed48ddcc8ddbc1ee55efd50051c6298a3af570bc
Almost Engineer <bitsecondal@gmail.com>
2019-05-05 06:20:51 -0500

    #8 Add handle exception

--- src/main/java/com/thealmostengineer/drupal7/webdriver/PropertiesFileHandler.java
+++ src/main/java/com/thealmostengineer/drupal7/webdriver/PropertiesFileHandler.java
@@ -1,18 +1,30 @@
 package com.thealmostengineer.drupal7.webdriver;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesFileHandler {
-	Properties readPropertyFile(String fileName) throws IOException {
-		App.logMessage("Loading properties file " + fileName);
+	Properties readPropertyFile(String filePath) {
+		App.logMessage("Loading properties file " + filePath);
 		Properties properties = new Properties();
-		File file = new File(fileName);
-		FileReader fileReader = new FileReader(file);
+		File file = new File(filePath);
+		FileReader fileReader = null;
 		
-		properties.load(fileReader);
+		try {
+			fileReader = new FileReader(file);
+			properties.load(fileReader);
+			fileReader.close(); // close properties file
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			properties = null;
+		} catch(IOException e) {
+			e.printStackTrace();
+			properties = null;
+		} // end try
+		
 		return properties;
 	} // end function
 }
